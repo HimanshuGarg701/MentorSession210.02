a92387fb6b6132f5752a97ebcfa7244a330e0635
Himanshu Garg <hgarg701@gmail.com>
2021-02-12 14:59:38 -0800

    February 12 (2:30PM TO 3:00 PM) mENTOR Session

---
+++ MentorSesFridayTwo.java
@@ -0,0 +1,24 @@
+public class MentorSesFridayTwo {
+    public static void main(String[] args){
+        int x = 5;
+        int y = 6;
+        int z = x*y;
+        int addition = x+y;
+        System.out.println(addition);
+        x = 7;
+        addition = x+y;
+        System.out.println(addition);
+
+
+        double decimalNum = 1.5;
+        double decimalVlaueTwo = 2.5;
+
+        char character = '!';
+
+        String word = "Hello There";
+
+        System.out.println(word);
+        System.out.println(decimalNum);
+        System.out.println(z);
+    }
+}
