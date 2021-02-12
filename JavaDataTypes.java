b954b4b9242ae78855c4273842d7ec68796002f7
Himanshu Garg <hgarg701@gmail.com>
2021-02-12 14:58:57 -0800

    February 12 (2:00PM TO 2:30 PM) mENTOR Session

---
+++ JavaDataTypes.java
@@ -0,0 +1,28 @@
+public class JavaDataTypes {
+    public static void main(String[] args){
+        byte num1 = 120; //less memory -127 to 128
+        short num2 = 20; //-32728 to 32728
+        long num3 = 20; // -9 billion to 9 billion
+        int x = 20; // -2 billiob to 2 billion
+
+
+
+
+        int y = 6;
+        int z = x+y;
+        System.out.println(z);
+        int product = x*y;
+        x = 6;
+        z = x+y;
+        System.out.println(z);
+        double decimalNumber = 1.5;
+        char character = 'A';
+        String word = "Hello There";
+        System.out.println("hello there");
+        System.out.println(character);
+        System.out.println(z);
+        System.out.println(decimalNumber);
+
+        // Data Types
+    }
+}
